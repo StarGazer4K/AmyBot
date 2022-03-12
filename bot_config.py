a1e720824cd039cf9e4e782b3e86af53511ca1f2
StarGazer4K <57087368+StarGazer4K@users.noreply.github.com>
2022-03-12 11:52:21 +0800

    Add files via upload

---
+++ bot_config.py
@@ -0,0 +1,10 @@
+from nonebot.default_config import *
+from datetime import timedelta
+
+SUPERUSERS={}#qq号
+COMMAND_START={''}
+NICKNAME={'。','.'}
+SESSION_EXPIRE_TIMEOUT=timedelta(minutes=2)
+HOST='127.0.1.1'
+Port=8080
+DEBUG = False
